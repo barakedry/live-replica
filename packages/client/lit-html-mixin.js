d16e01a0d311aae5c30797ae0f1880c9869e3618
barakedry <bara.edry@ebs.com>
2018-07-25 16:44:13 +0300

    lit html mixin work

--- packages/client/lit-html-mixin.js
+++ packages/client/lit-html-mixin.js
@@ -2,11 +2,14 @@ const Replica = require('@live-replica/replica');
 
 function createDirective(replica, property) {
 
-    const subscribersByPart = new WeakSet();
+    const subscribersByPart = new WeakMap();
 
     const directive = (part) => {
 
-        if (!subscribersByPart.has(part)) {
+
+        if (subscribersByPart.has(part)) {
+            part.setValue(replica.get(property));
+        } else {
 
             const unsub = replica.subscribe((diff) => {
                 if (diff[property]) {
@@ -21,64 +24,64 @@ function createDirective(replica, property) {
     return directive;
 }
 
+function lr(replica, path) {
+    if (!this.__replicaDirectivesCache) {
+        this.__replicaDirectivesCache = new WeakMap();
+    }
+
+    const lastPart = path.lastIndexOf('.');
+    let property;
+    if (lastPart === -1) {
+        property = path;
+
+    } else {
+        property = path.substr(lastPart + 1);
+        path = path.substr(0, lastPart);
+        replica = replica.at(path);
+    }
+
+    let replicasDirectives = this.__replicaDirectivesCache.get(replica);
+    if (!replicasDirectives) {
+        replicasDirectives = {};
+        this.__replicaDirectivesCache.set(replica, replicasDirectives);
+    }
+
+    if (!replicasDirectives[property]) {
+        replicasDirectives[property] = createDirective(replica, property);
+    }
+
+    return replicasDirectives[property];
+}
+
+lr.get = function  (pathOrBaseReplica) {
+    let replica;
+    if (typeof pathOrBaseReplica === 'string') {
+        replica = new Replica(pathOrBaseReplica);
+    } else {
+        replica = pathOrBaseReplica;
+    }
+
+    if (!this.__replicas) {
+        this.__replicas = [];
+    }
+
+    this.__replicas.push(replica);
+
+    return replica;
+};
+
 module.exports = function LitHtmlMixin(base) {
     return class extends base {
 
-        liveReplica(pathOrBaseReplica) {
-            let replica;
-            if (typeof pathOrBaseReplica === 'string') {
-                replica = new Replica(pathOrBaseReplica);
-            } else {
-                replica = pathOrBaseReplica;
-            }
-
-            if (!this.__replicas) {
-                this.__replicas = [];
-            }
-
-            this.__replicas.push(replica);
-
-            return replica;
+        constructor() {
+            super();
+            this.lr = lr.bind(this);
+            this.lr.get = lr.get.bind(this);
+            // this.lr.watch = lr.watch.bind(this);
         }
 
         disconnectedCallback() {
             super.disconnectedCallback();
-            if (this.__replicas) {
-                this.__replicas.forEach((replica) => {
-                    replica.disconnect();
-                });
-            }
         };
-
-        $LR (replica, path) {
-
-            if (!this.__replicaDirectivesCache) {
-                this.__replicaDirectivesCache = new WeakSet();
-            }
-
-            const lastPart = path.lastIndexOf('.');
-            let property;
-            if (lastPart === -1) {
-                property = path;
-
-            } else {
-                property = path.substr(lastPart);
-                path = path.substr(0, lastPart);
-                replica = replica.at(path);
-            }
-
-            let replicasDirectives = this.__replicaDirectivesCache.get(replica);
-            if (!replicasDirectives) {
-                replicasDirectives = {};
-                this.__replicaDirectivesCache.set(replica, replicasDirectives);
-            }
-
-            if (!replicasDirectives[property]) {
-                replicasDirectives[property] = createDirective(replica, property);
-            }
-
-            return replicasDirectives[property];
-        }
-
     };
 };
