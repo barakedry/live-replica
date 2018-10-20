beb024fb721bd0c588e48eb27a381a61e7ad3444
Barak Edry <barak.edry@ebs.com>
2018-10-21 01:43:05 +0300

    exposed some classes for node usage

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-if (typedef window === 'undefined') {
-
-}
+module.exports = {
+    Server: require('./packages/server'),
+    Proxy: require('./packages/proxy'),
+    Replica: require('./packages/replica'),
+    PatchDiff: require('./packages/patch-diff')
+};
