5c3ca08d8ab2d4b97452f716a378aa74c821bb1b
Barak Edry <barak.edry@ebs.com>
2019-06-13 21:52:39 +0300

    added the whitelist middleware

--- packages/server/src/middlewares.js
+++ packages/server/src/middlewares.js
@@ -3,60 +3,78 @@
  */
 'use strict';
 
+function whitelist(list) {
+    if (Array.isArray(list)) {
+        list = new Set(list);
+    } else if (!(list instanceof Set)) {
+        throw new Error('TypeError "list" must be an Array or a Set');
+    }
+
+    return function check(request, reject, approve) {
+        if (list.has(request.path)) {
+            approve(request);
+        } else {
+            reject(`Unauthorized subscription to "${request.path}"`);
+        }
+    }
+}
+
 const subscriptionCounter = new WeakMap();
+function oncePerSubscription(path, firstSubscriptionCallback, lastSubscriptionCallback) {
 
-module.exports = {
-    oncePerSubscription(path, firstSubscriptionCallback, lastSubscriptionCallback) {
+    if (typeof path === 'function') {
+        lastSubscriptionCallback = firstSubscriptionCallback;
+        firstSubscriptionCallback = path;
+        path = undefined;
+    }
 
-        if (typeof path === 'function') {
-            lastSubscriptionCallback = firstSubscriptionCallback;
-            firstSubscriptionCallback = path;
-            path = undefined;
+    return function onSubscribe(request, reject, approve) {
+        const server = this;
+
+        if (path && request.path !== path) {
+            return approve();
         }
 
-        return function onSubscribe(request, reject, approve) {
-            const server = this;
+        if (!subscriptionCounter.has(this)) {
+            subscriptionCounter.set(this, {});
+        }
 
-            if (path && request.path !== path) {
-                return approve();
-            }
+        const subscribePath = request.path;
+        const subscribed = subscriptionCounter.get(this);
+        if (!subscribed.hasOwnProperty(subscribePath)) {
+            subscribed[subscribePath] = 0;
+        }
 
-            if (!subscriptionCounter.has(this)) {
-                subscriptionCounter.set(this, {});
-            }
+        if (subscribed[subscribePath] === 0) {
+            server.on('replica-unsubscribe', function onUnsubscribe(unsubscriberRequest)  {
+                if (subscribePath === unsubscriberRequest.path) {
 
-            const subscribePath = request.path;
-            const subscribed = subscriptionCounter.get(this);
-            if (!subscribed.hasOwnProperty(subscribePath)) {
-                subscribed[subscribePath] = 0;
-            }
-
-            if (subscribed[subscribePath] === 0) {
-                server.on('replica-unsubscribe', function onUnsubscribe(unsubscriberRequest)  {
-                    if (subscribePath === unsubscriberRequest.path) {
-
-                        if (!subscribed[subscribePath]) {
-                            assert('')
-                        }
-
-                        subscribed[subscribePath]--;
-
-                        if (subscribed[subscribePath] <= 0) {
-                            delete subscribed[subscribePath];
-                            server.removeListener('replica-unsubscribe', onUnsubscribe);
-                            if (lastSubscriptionCallback) {
-                                lastSubscriptionCallback.call(server, unsubscriberRequest);
-                            }
-
-                        }
+                    if (!subscribed[subscribePath]) {
+                        assert('')
                     }
-                });
 
-                firstSubscriptionCallback.call(server, request, reject, approve);
-            }
+                    subscribed[subscribePath]--;
 
-            subscribed[request.path]++;
-            approve();
-        };
-    }
+                    if (subscribed[subscribePath] <= 0) {
+                        delete subscribed[subscribePath];
+                        server.removeListener('replica-unsubscribe', onUnsubscribe);
+                        if (lastSubscriptionCallback) {
+                            lastSubscriptionCallback.call(server, unsubscriberRequest);
+                        }
+
+                    }
+                }
+            });
+
+            firstSubscriptionCallback.call(server, request, reject, approve);
+        }
+
+        subscribed[request.path]++;
+        approve();
+    };
+}
+
+module.exports = {
+    oncePerSubscription,
+    whitelist
 };
