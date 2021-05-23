ae7caef12b97af5433b3e5df4bbb05d1f5e22e09
Barak Edry <barak.edry@gmail.com>
2021-05-24 01:15:50 +0300

    added suppport for node js cluster

---
+++ packages/cluster-worker-socket/cluster-worker-socket.js
@@ -0,0 +1,77 @@
+/**
+ * Created by barakedry on 06/07/2018.
+ */
+'use strict';
+const LiveReplicaEvents = require('../common/events');
+const Events = require('events');
+const LiveReplicaSocket = require('../socket');
+const cluster = require('cluster');
+let acks = 1;
+/**
+ *  LiveReplicaWorkerSocket
+ */
+class LiveReplicaClusterWorkerSocket extends LiveReplicaSocket {
+
+    static get instance() {
+        if (!this._instance) {
+            this._instance = new LiveReplicaClusterWorkerSocket();
+        }
+
+        return this._instance;
+    }
+
+    constructor() {
+        if (!cluster.isWorker) {
+            throw new Error('LiveReplicaClusterWorkerSocket can be initiated only on a node.js cluster worker process')
+        }
+        super();
+        this._emitter = new Events.EventEmitter();
+        this._emitter.setMaxListeners(50000);
+
+        process.on('message', (msg) => {
+            if (typeof msg === 'object' && msg.liveReplica) {
+                const {event, args} = msg.liveReplica;
+                this._emitter.emit(event, ...args);
+            }
+        });
+    }
+
+    // overrides
+
+    _addSocketEventListener(eventName, fn) {
+        this._emitter.on(eventName, fn);
+    }
+    _addSocketEventListenerOnce(eventName, fn) {
+        this._emitter.once(eventName, fn);
+    }
+
+    _removeSocketEventListener(eventName, fn) {
+        this._emitter.removeListener(eventName, fn);
+    }
+
+    _socketSend(event, payload, ack) {
+        let ackEvent;
+        if (ack) {
+            ackEvent = `lr-acks::${++acks}`;
+            this.once(ackEvent, ack);
+        }
+
+        const message = {
+            liveReplica: {
+                event,
+                payload,
+                ack: ackEvent,
+            }
+        };
+
+        process.send(message);
+    }
+
+    get baseSocket() {
+        return process;
+    }
+
+    isConnected() { return true; }
+}
+
+module.exports = LiveReplicaClusterWorkerSocket;
