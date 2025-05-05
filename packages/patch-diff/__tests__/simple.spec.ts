2ce5125da413825c8a3c94f159043d54b2a8d166
odfalik <falik.oded.1@gmail.com>
2025-05-04 19:12:51 -0700

    wrap simple.spec.ts in a test harness

--- packages/patch-diff/__tests__/simple.spec.ts
+++ packages/patch-diff/__tests__/simple.spec.ts
@@ -102,50 +102,46 @@ deepPatch.a.b.c6 = structuredClone(deepPatch);
 // @ts-expect-error
 deepPatch.a.b.c7 = structuredClone(deepPatch);
 
+describe('PatchDiff simple usage', () => {
+    it('should perform PatchDiff operations without error', () => {
+        let pd = new PatchDiff(structuredClone(deepPatch));
+        //console.log(JSON.stringify(pd.get()));
+        pd.subscribe('', () => {});
+        pd.subscribe('a', () => {});
+        pd.subscribe('a', (diff) => {});
+        pd.subscribe('a.b', () => {});
+        pd.subscribe('a.b.c', () => {});
+        pd.subscribe('a.b.c.d.inner.a.b.c.d.inner.a.b.c.d', (diff) => console.log('sub a.b.c.d.inner.a.b.c.d.inner.a.b.c.d =>', JSON.stringify(diff)));
 
-let pd = new PatchDiff(structuredClone(deepPatch));
-//console.log(JSON.stringify(pd.get()));
-pd.subscribe('', () => {});
-pd.subscribe('a', () => {});
-pd.subscribe('a', (diff) => {});
-pd.subscribe('a.b', () => {});
-pd.subscribe('a.b.c', () => {});
-pd.subscribe('a.b.c.d.inner.a.b.c.d.inner.a.b.c.d', (diff) => console.log('sub a.b.c.d.inner.a.b.c.d.inner.a.b.c.d =>', JSON.stringify(diff)));
+        console.info('======== mutating data ========');
+        console.time('displace');
+        pd.at('a.b').displace(pd.options.deleteKeyword);
+        console.timeEnd('displace');
+        //console.log(JSON.stringify(pd.get()));
 
-console.info('======== mutating data ========');
-console.time('displace');
-pd.at('a.b').displace(pd.options.deleteKeyword);
-console.timeEnd('displace');
-//console.log(JSON.stringify(pd.get()));
+        console.warn("======= remove =======");
 
+        pd = new PatchDiff(structuredClone(deepPatch));
+        //console.log(JSON.stringify(pd.get()));
+        pd.subscribe('', () => {});
+        pd.subscribe('a', () => {});
+        pd.subscribe('a', (diff) => {});
+        pd.subscribe('a.b', () => {});
+        pd.subscribe('a.b.c', () => {});
+        pd.subscribe('a.b.c.d.inner.a.b.c.d.inner.a.b.c.d', (diff) => console.log('sub a.b.c.d.inner.a.b.c.d.inner.a.b.c.d =>', JSON.stringify(diff)));
 
-console.warn("======= remove =======");
+        console.info('======== mutating data ========');
+        console.time('remove');
+        pd.at('a.b').remove();
+        console.timeEnd('remove');
+        //console.log(JSON.stringify(pd.get()));
 
-
-pd = new PatchDiff(structuredClone(deepPatch));
-//console.log(JSON.stringify(pd.get()));
-pd.subscribe('', () => {});
-pd.subscribe('a', () => {});
-pd.subscribe('a', (diff) => {});
-pd.subscribe('a.b', () => {});
-pd.subscribe('a.b.c', () => {});
-pd.subscribe('a.b.c.d.inner.a.b.c.d.inner.a.b.c.d', (diff) => console.log('sub a.b.c.d.inner.a.b.c.d.inner.a.b.c.d =>', JSON.stringify(diff)));
-
-console.info('======== mutating data ========');
-console.time('remove');
-pd.at('a.b').remove();
-console.timeEnd('remove');
-//console.log(JSON.stringify(pd.get()));
-
-
-
-
-
-console.info('======== test apply ========');
-for (let i = 0; i < 10; i++) {
-    const pdPerf = new PatchDiff(structuredClone(data));
-    console.time(`apply ${i}`);
-    pdPerf.apply(deepPatch);
-    console.timeEnd(`apply ${i}`);
-
-}
+        console.info('======== test apply ========');
+        for (let i = 0; i < 10; i++) {
+            const pdPerf = new PatchDiff(structuredClone(data));
+            console.time(`apply ${i}`);
+            pdPerf.apply(deepPatch);
+            console.timeEnd(`apply ${i}`);
+        }
+    });
+});
